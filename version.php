681f12d2e731255fb969984997308189b463ac29
Evgeniy <75742491+evgeniy-vostrickov@users.noreply.github.com>
2024-05-24 12:36:01 +0300

    Update version.php

--- version.php
+++ version.php
@@ -9,6 +9,6 @@
  * @license    -
  */
 
-$plugin->version = 2022101900;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2024050100;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2016060900; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->component = 'local_course_recommendation'; // Full name of the plugin (used for diagnostics)
